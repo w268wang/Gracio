bc9b40c21b628b608635b4cdc0dbb3ead54ade8e
Weijie Wang <wwang@v1020-wn-153-232.campus-dynamic.uwaterloo.ca>
2015-09-19 17:03:32 -0400

    rename maing.py

--- __init__.py
+++ __init__.py
@@ -4,10 +4,14 @@ from flask import Flask, Response, request, make_response, redirect
 
 import services.mongo_service as mongo
 
-import requests, json
+import requests, json, os
 
+AWS_IP_ADDRESS = "54.152.97.131"
+BASE_URL = "http://localhost:5000"
 
-BASE_URL = "http://127.0.0.1:5000"
+if urllib2.urlopen('http://ip.42.pl/raw').read() == AWS_IP_ADDRESS:
+    log.startLogging(open('/var/log/gracio/gracio.log', 'w'))
+    BASE_URL = "http://" + mongo.AWS_IP_ADDRESS
 
 FB_APP_ID = "1113294778698674"
 FB_APP_SECRET = "fd70f888513b16e6825523618ae5bf18"
@@ -17,10 +21,27 @@ fb_debug_token_url = "https://graph.facebook.com/debug_token?"
 
 app = Flask(__name__)
 
+# TODO: address? post code : detailed address; <- let user type in when they want to request or provide a ride
+#       add provider
+#       add consumer
+
+
+def get_file(filename):  # pragma: no cover
+    try:
+        src = os.path.join(os.path.abspath(os.path.dirname(__file__)), filename)
+        # Figure out how flask returns static files
+        # Tried:
+        # - render_template
+        # - send_file
+        # This should not be so non-obvious
+        return open(src).read()
+    except IOError as exc:
+        return str(exc)
 
 @app.route("/", methods=['GET'])
 def index():
-    return Response("abc", mimetype="text/html")
+    content = get_file('template/index.html')
+    return Response(content, mimetype="text/html")
 
 
 # call when the sign in with fb button gets clicked
@@ -66,7 +87,7 @@ def get_fb_credentials():
     if not user_exists:
         mongo.add_facebook_user(user_id)
 
-    app_callback_url = BASE_URL + "/mobile"
+    app_callback_url = BASE_URL
 
     res = make_response(redirect(app_callback_url))
     res.set_cookie("status", "0")
@@ -75,14 +96,38 @@ def get_fb_credentials():
 
 # to fill in information like phone number and email (since in fb_callback,
 # each user only have userid set)
-#   Example: /fill_user?user_id=test&phong_number=1234567890&email=example@test.com
+#   Example: /fill_user?user_id=test&phong_number=1234567890&email=example@test.com&nickname=test_nick_name
 @app.route("/fill_user")
 def fill_user():
     user_id = request.args.get("user_id")
     phone_number = request.args.get("phone_number")
     email = request.args.get("email")
+    nickname = request.args.get("nickname")
 
-    mongo.fill_facebook_user(user_id, phone_number, email)
+    mongo.fill_facebook_user(user_id, phone_number, email, nickname)
+    return json.dumps({"status": 200})
+
+
+@app.route("/provide_ride")
+def provide_ride():
+    user_id = request.args.get("user_id")
+    time = request.args.get("time")
+    target_address = request.args.get("target_address")
+    quantity = request.args.get("quantity")
+
+    mongo.add_provide_info(user_id, time, target_address, quantity)
+    return json.dumps({"status": 200})
+
+
+@app.route("/request_ride")
+def request_ride():
+    user_id = request.args.get("user_id")
+    time = request.args.get("time")
+    target_address = request.args.get("target_address")
+    quantity = request.args.get("quantity")
+
+    mongo.add_provide_info(user_id, time, target_address, quantity)
+    return json.dumps({"status": 200})
 
 
 if __name__ == "__main__":
